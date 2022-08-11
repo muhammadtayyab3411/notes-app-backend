cc329bf0889e968a28bc2451fce692a6c4eb1cfa
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-08-11 15:34:46 +0500

    hosting to heroku

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const DB_USER_NAME = process.env.DB_USER_NAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const DBUSERNAME = process.env.DBUSERNAME;
+const DBPASSWORD = process.env.DBPASSWORD;
 
-const db_conn_url = `mongodb://${DB_USER_NAME}:${DB_PASSWORD}@ac-2relq0c-shard-00-00.of949mg.mongodb.net:27017,ac-2relq0c-shard-00-01.of949mg.mongodb.net:27017,ac-2relq0c-shard-00-02.of949mg.mongodb.net:27017/notes-backend?ssl=true&replicaSet=atlas-pr1dpn-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const db_conn_url = `mongodb://${DBUSERNAME}:${DBPASSWORD}@ac-2relq0c-shard-00-00.of949mg.mongodb.net:27017,ac-2relq0c-shard-00-01.of949mg.mongodb.net:27017,ac-2relq0c-shard-00-02.of949mg.mongodb.net:27017/notes-backend?ssl=true&replicaSet=atlas-pr1dpn-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 mongoose
   .connect(db_conn_url)
