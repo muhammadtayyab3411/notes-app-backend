4d21aa6457acdc87c5d5ecbf0d267294f1ce5b8a
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-08-11 15:15:33 +0500

    mongodb atlas added

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const DB_USER_NAME = process.env.DB_USER_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const db_conn_url = `mongodb://${DB_USER_NAME}:${DB_PASSWORD}@ac-2relq0c-shard-00-00.of949mg.mongodb.net:27017,ac-2relq0c-shard-00-01.of949mg.mongodb.net:27017,ac-2relq0c-shard-00-02.of949mg.mongodb.net:27017/notes-backend?ssl=true&replicaSet=atlas-pr1dpn-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 mongoose
-  .connect("mongodb://localhost:27017/mynotes")
+  .connect(db_conn_url)
   .then(() => {
     console.log("Connection to db successfull");
   })
