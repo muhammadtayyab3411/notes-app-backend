60539a53769e9425bb2a9ec5dd055e110f0e9eb2
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-08-23 13:13:25 +0500

    testing

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,10 +19,10 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const auth_token = req.cookies.auth_token;
-    if (auth_token) {
-      res.status(403).send({ error: "You are already signed in!" });
-    }
+    // const auth_token = req.cookies.auth_token;
+    // if (auth_token) {
+    //   res.status(403).send({ error: "You are already signed in!" });
+    // }
 
     const { name, user_name, email, password, confirm_password } = req.body;
     try {
