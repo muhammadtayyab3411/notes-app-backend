b417946e47a18fa69ac6fcf7fde0ac5ea1aad3db
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-10-08 00:40:43 +0500

    minor fixes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000/login",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
